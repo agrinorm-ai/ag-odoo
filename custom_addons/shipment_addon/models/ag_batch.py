1e34e3b4afe80083c236c4cc94c8c8e52d4d74e7
Fernando Calvo <fernando.calvo@agrinorm.ai>
2024-07-03 09:02:21 +0200

    Model batch parent_id correctly

--- custom_addons/shipment_addon/models/ag_batch.py
+++ custom_addons/shipment_addon/models/ag_batch.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 class AgBatch(models.Model):
     _name = 'ag.batch'
     _description = 'Batch'
-
-    parent_id = fields.Char(string='Parent ID')
-    article_id = fields.Many2one('product.product', string='Article ID')
+    
+    parent_id = fields.Many2one('ag.batch', string='Parent ID', ondelete='restrict') 
+    child_ids = fields.One2many('ag.batch', 'parent_id', string='Child IDs')
